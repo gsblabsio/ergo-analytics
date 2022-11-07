8be11801f094bd4af439f4019bde6416be77a04d
Walter Bodwell <wbodwell@planigle.com>
2022-11-07 17:55:13 -0600

    GSB-151 - Fix redis dependency

--- chain-grabber/docker-entrypoint.sh
+++ chain-grabber/docker-entrypoint.sh
@@ -5,6 +5,7 @@ sed -i "s/http:\/\/213.239.193.208:9053/http:\/\/node:9053/g" modules/chain-grab
 sed -i "s/jdbc:postgresql:\/\/localhost:5432\/explorer/jdbc:postgresql:\/\/postgres:5432\/explorer/g" modules/chain-grabber/src/main/resources/application.conf
 sed -i "s/db.user = \"postgres\"/db.user = \"$POSTGRES_USER\"/g" modules/chain-grabber/src/main/resources/application.conf
 sed -i "s/db.pass = \"1234\"/db.pass = \"$POSTGRES_PASSWORD\"/g" modules/chain-grabber/src/main/resources/application.conf
+sed -i "s/redis:\/\/localhost:6380/redis:\/\/redis:6379/g" modules/chain-grabber/src/main/resources/application.conf
 
 export PGPASSWORD="$POSTGRES_PASSWORD"
 
